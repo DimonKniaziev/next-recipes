d74eeeecc2ed325717dbbd52378cd24c7937f751
DimonKniaziev <dimkniazz@gmail.com>
2024-04-16 15:15:24 +0300

    add-firestore

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB1gXZuyvk4Q3CwYyoXrLLI52QAKI4Urik",
@@ -12,3 +13,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
+export const db = getFirestore(app)
